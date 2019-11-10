66769013b24b3342a8d0166ee47c8f5ac3e9ee99
Borek Bernard <borekb@gmail.com>
2019-11-10 11:12:56 +0100

    Add intro & Gatsby sections (rough draft)

---
+++ talk/theme.js
@@ -0,0 +1,17 @@
+// 'notes' theme, copied here for customizations
+// Source: https://github.com/jxnblk/mdx-deck/blob/508141d/packages/themes/notes.js
+export default {
+  googleFont: 'https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope',
+  fonts: {
+    body: '"Annie Use Your Telescope", cursive',
+  },
+  styles: {
+    root: {
+      fontSize: ['1.5em', null, '4em'],
+      textAlign: 'center',
+    },
+    pre: {
+      textAlign: 'left',
+    },
+  },
+};
